b9b38577c61632d10ef3cf527765edc396c50571
Constantine Karnaukhov <1119@zolotoykod.ru>
2018-02-15 10:42:31 +0400

    feat(AppConfig): add set method

--- src/Bitrix/AppConfig.php
+++ src/Bitrix/AppConfig.php
@@ -8,36 +8,37 @@ class AppConfig {
 	private $instance = null;
 	private $options = [];
 	private $values = [];
+	private $appConfig = [];
 
 	/**
 	 * AppConfig constructor.
 	 * @param array $options
 	 * @throws \Bitrix\Main\LoaderException
 	 */
-	public function __construct(array $options) {
+	public function __construct(array $options = []) {
 		if ($this->instance) return $this->instance;
+
+		Loader::includeModule('iblock');
 
 		$this->options = array_merge($this->options, [
 			'iblockCode' => 'AppConfig',
 			'configProps' => []
 		], $options);
 
-		$this->values = $this->init();
+		$this->init();
 
 		$this->instance = $this;
 		return $this;
 	}
 
 	/**
-	 * Get app config object-like array
-	 * @return array
+	 * Init values array
 	 * @throws \Bitrix\Main\LoaderException
 	 */
-	public function init() {
-		Loader::includeModule('iblock');
+	private function init() {
 		$res = [];
 
-		$appConfig = \CIBlockElement::GetList(
+		$this->appConfig = \CIBlockElement::GetList(
 			['ACTIVE' => 'Y'],
 			['IBLOCK_CODE' => $this->options['iblockCode']],
 			false,
@@ -47,11 +48,32 @@ class AppConfig {
 			}, $this->options['configProps'])
 		)->Fetch();
 
-		foreach ($this->options['configProps'] as $key) {
-			$res[$key] = $appConfig['PROPERTY_' . $key . '_VALUE'];
+		if (!$this->appConfig) {
+			$this->create();
+			return;
 		}
 
-		return $res;
+		foreach ($this->options['configProps'] as $key) {
+			$res[$key] = $this->appConfig['PROPERTY_' . $key . '_VALUE'];
+		}
+
+		$this->values = $res;
+	}
+
+	/**
+	 * Create AppConfig
+	 * @throws \Bitrix\Main\LoaderException
+	 */
+	private function create() {
+		$iblockId = IblockUtility::getIblockIdByCode($this->options['iblockCode']);
+		(new \CIBlockElement())->Add([
+			'IBLOCK_ID' => $iblockId,
+			'CREATED_BY' => 1,
+			'MODIFIED_BY' => 1,
+			'NAME' => 'Settings',
+			'ACTIVE' => 'Y'
+		]);
+		$this->init();
 	}
 
 	/**
@@ -63,16 +85,31 @@ class AppConfig {
 	 */
 	public function get($prop, $default = '', callable $transform = null) {
 		if (is_null($transform)) $transform = function($a) {return $a;};
-		$val = $this->values[$prop];
+		$val = $this->appConfig['PROPERTY_' . $prop . '_VALUE'];
 		return call_user_func($transform, $val ? $val : $default);
 	}
 
 	/**
-	 * Return all values
+	 * Get AppConfig
 	 * @return array
 	 */
-	public function getValues()
+	public function getAppConfig()
 	{
-		return $this->values;
+		return $this->appConfig;
+	}
+
+	/**
+	 * Set config property
+	 * @param string $prop property name
+	 * @param mixed $value new property value
+	 * @return boolean
+	 */
+	public function set($prop, $value)
+	{
+		return (new \CIBlockElement())->SetPropertyValueCode(
+			$this->appConfig['ID'],
+			$prop,
+			$value
+		);
 	}
 }
